2d8540af4790d9700e870c323e9ed20f63c3c731
sho.nagato <s.nagato-good-works.co.jp>
2021-01-21 12:50:23 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Singleton instance1 = Singleton.getInstance();
+        Singleton instance2 = Singleton.getInstance();
+
+        if (instance1 == instance2) {
+            System.out.println("instance1とinstance2は同じインスタンスです。");
+        } else {
+            System.out.println("instance1とinstance2は違うインスタンスです。");
+        }
+    }
+}
