53a1b99302099e3f714c933140dbe9b9c664e9b3
Sho Nagato <nagato.sho.178@gmail.com>
2023-07-11 16:29:37 +0900

    add console output

--- src/Singleton.java
+++ src/Singleton.java
@@ -1,7 +1,9 @@
 public class Singleton {
     private static Singleton instance = new Singleton();
 
-    private Singleton() {}
+    private Singleton() {
+        System.out.println("インスタンスを生成しました。");
+    }
 
     public static Singleton getInstance() {
         return instance;
